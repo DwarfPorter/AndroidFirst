6977dd30387e53fdc44cf3be34ea469734197f79
MixidFinder <mixidfinder@mail.ru>
2021-05-29 16:47:20 +0300

    HW1
    complite all hw

--- app/src/main/java/com/example/lesson1/MainActivity.java
+++ app/src/main/java/com/example/lesson1/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.lesson1;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,15 +14,19 @@ import android.widget.TextView;
 
 import java.text.BreakIterator;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Integer arg1;
     private Integer arg2;
+    Button btnActTwo;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
 
         EditText editText1 = findViewById(R.id.edit1);
         EditText editText2 = findViewById(R.id.edit2);
@@ -41,5 +49,13 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnActTwo) {
+            Intent intent = new Intent(this, ActivityTwo.class);
+            startActivity(intent);
+        }
     }
 }
