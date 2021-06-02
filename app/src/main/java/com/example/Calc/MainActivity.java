94a062d073ce80b3064f22296cf50bd4b41baeec
MixidFinder <mixidfinder@mail.ru>
2021-06-02 19:13:56 +0300

    HW2 fix and renamed

--- app/src/main/java/com/example/Calc/MainActivity.java
+++ app/src/main/java/com/example/Calc/MainActivity.java
@@ -1,14 +1,13 @@
-package com.example.lesson1;
+package com.example.Calc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ActivityTwo extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_two);
+        setContentView(R.layout.activity_main);
     }
 }
